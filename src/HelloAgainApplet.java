4ea9370fb0bb8263256fbfae5a3a701090d8674e
amrshokry84 <amr.shokry84@gmail.com>
2017-01-30 11:37:55 +0200

    First Commit

---
+++ src/HelloAgainApplet.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics; 
+import java.awt.Font; 
+import java.awt.Color;
+
+public class HelloAgainApplet extends java.applet.Applet  {
+
+	private static final long serialVersionUID = 1L;
+	
+	Font f = new Font("TimesRoman",Font.BOLD,36);
+	
+	public void paint(Graphics g) {    
+		g.setFont(f);    
+	    g.setColor(Color.red);    
+	    g.drawString("Hello again!", 10, 50); 
+	    }
+}
